c417c220530bafd439c56705d3e28c55baa6ede5
Camilo3rd <camilord@abcs.co.nz>
2018-03-24 22:46:32 +1300

    add new functions

--- src/Net/EmailUtilus.php
+++ src/Net/EmailUtilus.php
@@ -137,4 +137,13 @@ class EmailUtilus
     public static function isValidEmail($email) {
         return filter_var($email, FILTER_VALIDATE_EMAIL);
     }
+
+    public static function obfuscateEmail($email)
+    {
+        $em   = explode("@",$email);
+        $name = implode(array_slice($em, 0, count($em)-1), '@');
+        $len  = floor(strlen($name)/2);
+
+        return substr($name,0, $len) . str_repeat('*', $len) . "@" . end($em);
+    }
 }
