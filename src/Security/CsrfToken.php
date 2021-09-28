7f7b2993d2b281251dffa2de4c5634e48e146e32
Camilo Lozano III <camilo.lozano@objective.com>
2021-09-29 12:57:25 +1300

    improve csrf generation and added gc cleaner for the csrf sessions

--- src/Security/CsrfToken.php
+++ src/Security/CsrfToken.php
@@ -14,11 +14,11 @@ use camilord\utilus\Encryption\DataCryptInterface;
  */
 class CsrfToken
 {
-    private $expiration = 3600;
+    private $expiration;
     private $key;
     private $secret_key;
 
-    const PREFIX = '_csrf_';
+    const PREFIX = '_csrf_sessions';
 
     /**
      * @var DataCryptInterface
@@ -51,6 +51,12 @@ class CsrfToken
         if (php_sapi_name() !== 'cli' && session_status() !== 2) {
             session_start();
         }
+
+        if (!isset($_SESSION[self::PREFIX])) {
+            $_SESSION[self::PREFIX] = [];
+        }
+
+        $this->clean_csrf();
     }
 
     /**
@@ -59,14 +65,21 @@ class CsrfToken
      */
     public function generate(string $identify): string
     {
-        $csrf_token = [
+        $csrf_token_data = [
             'uuid' => UUID::v4(),
             'expire' => (time() + $this->expiration),
             'hash' => sha1(time())
         ];
-        $_SESSION[self::PREFIX.$identify] = $this->getEncoder()->encode(json_encode($csrf_token, JSON_PRETTY_PRINT));
 
-        return $_SESSION[self::PREFIX.$identify];
+        if (!isset($_SESSION[self::PREFIX][$identify])) {
+            $_SESSION[self::PREFIX][$identify] = [];
+        }
+
+        $csrf_token = $this->getEncoder()->encode(json_encode($csrf_token_data, JSON_PRETTY_PRINT));
+        $hash_token = sha1($csrf_token);
+        $_SESSION[self::PREFIX][$identify][$hash_token] = $csrf_token;
+
+        return $_SESSION[self::PREFIX][$identify][$hash_token];
     }
 
     /**
@@ -77,11 +90,14 @@ class CsrfToken
     public function is_csrf_valid(string $identify, string $submitted_token): bool
     {
         $server_token = [];
-        if (isset($_SESSION[self::PREFIX.$identify])) {
-            $server_token = json_decode($this->getEncoder()->decode($_SESSION[self::PREFIX.$identify]), true);
+        $hash_token = sha1($submitted_token);
+        if (isset($_SESSION[self::PREFIX][$identify][$hash_token])) {
+            $csrf_token = $_SESSION[self::PREFIX][$identify][$hash_token];
+            $server_token = json_decode($this->getEncoder()->decode($csrf_token), true);
         }
+
         $submitted_data = json_decode($this->getEncoder()->decode($submitted_token), true);
-        $this->setErrorMessage('Error! Invalid or missing CSRF token.');
+        $this->setErrorMessage('Error! CSRF token mismatch.');
 
         if (ArrayUtilus::haveData($submitted_data) && ArrayUtilus::haveData($server_token)) {
             if (
@@ -89,9 +105,9 @@ class CsrfToken
                 isset($server_token['hash']) && isset($submitted_data['hash']) &&
                 $server_token['uuid'] === $submitted_data['uuid'] &&
                 $server_token['hash'] === $submitted_data['hash'] &&
-                $submitted_data['expire'] >= time()
+                (int)$submitted_data['expire'] >= time()
             ) {
-                $this->destroy($identify);
+                $this->destroy($identify, $submitted_token);
                 $this->setErrorMessage(null);
                 return true;
             }
@@ -106,21 +122,20 @@ class CsrfToken
 
     /**
      * @param string $identify
+     * @param string $csrf_token
      */
-    public function destroy(string $identify) {
-        unset($_SESSION[self::PREFIX.$identify]);
+    public function destroy(string $identify, string $csrf_token) {
+        $hash_token = sha1($csrf_token);
+        unset($_SESSION[self::PREFIX][$identify][$hash_token]);
+        $this->clean_csrf();
     }
 
     /**
      * destroy all csrf tokens
      */
     public function destroy_all() {
-        if (ArrayUtilus::haveData($_SESSION)) {
-            foreach($_SESSION as $key => $val) {
-                if (stripos($key, self::PREFIX) !== false) {
-                    $this->destroy($_SESSION[$key]);
-                }
-            }
+        if (isset($_SESSION[self::PREFIX]) && ArrayUtilus::haveData($_SESSION[self::PREFIX])) {
+            $_SESSION[self::PREFIX] = [];
         }
     }
 
@@ -140,6 +155,40 @@ class CsrfToken
         }
 
         return $this->encoder;
+    }
+
+    /**
+     * clean up method for the sessions
+     */
+    private function clean_csrf()
+    {
+        if (ArrayUtilus::haveData($_SESSION[self::PREFIX]))
+        {
+            // delete expired csrf tokens
+            foreach($_SESSION[self::PREFIX] as $identify => $sessions) {
+                foreach($sessions as $hash_token => $csrf_token) {
+                    $csrf_data = json_decode($this->getEncoder()->decode($csrf_token), true);
+
+                    if (isset($csrf_data['expire']) && (int)$csrf_data['expire'] < time()) {
+                        unset($sessions[$hash_token]);
+                    }
+                }
+
+                // if there's too many records, deleted the old ones
+                if (ArrayUtilus::haveData($sessions)) {
+                    while(count($sessions) > 50) {
+                        array_shift($sessions);
+                    }
+                }
+
+                $_SESSION[self::PREFIX][$identify] = $sessions;
+
+                // if the area is empty array, delete the area
+                if (!ArrayUtilus::haveData($_SESSION[self::PREFIX][$identify])) {
+                    unset($_SESSION[self::PREFIX][$identify]);
+                }
+            }
+        }
     }
 
     /**
--- tests/Security/CsrfTokenTest.php
+++ tests/Security/CsrfTokenTest.php
@@ -3,6 +3,7 @@
 
 namespace camilord\utilus\Security;
 
+use camilord\utilus\Data\ArrayUtilus;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -21,6 +22,7 @@ class CsrfTokenTest extends TestCase
         return $obj;
     }
 
+    // test validate and must be invalid if revalidated again
     public function testCsrfValid() {
         $obj = $this->testObject();
         $identify = 'test_identity';
@@ -31,13 +33,14 @@ class CsrfTokenTest extends TestCase
         sleep(3);
 
         $actual = $obj->is_csrf_valid($identify, $csrf_token);
-        $this->assertTrue($actual);
+        $this->assertTrue($actual, '1:) '.$obj->getErrorMessage().' | '.print_r([$actual, $identify], true));
 
         // verify again, should be false because it been destroyed
         $actual = $obj->is_csrf_valid($identify, $csrf_token);
-        $this->assertFalse($actual);
+        $this->assertFalse($actual, '2:) '.$obj->getErrorMessage().' | '.print_r([$actual, $identify], true));
     }
 
+    // test expired validation
     public function testCsrfInvalid() {
         $obj = $this->testObject();
         $identify = 'test_identity';
@@ -48,5 +51,40 @@ class CsrfTokenTest extends TestCase
 
         $actual = $obj->is_csrf_valid($identify, $csrf_token);
         $this->assertFalse($actual);
+    }
+
+    public function testArrayedTokens() {
+        $obj = $this->testObject();
+
+        for ($i = 0; $i < 10; $i++) {
+            for ($j = 0; $j < 100; $j++) {
+                $identify = 'test_' . $i;
+                $csrf_token = $obj->generate('test_' . $i);
+
+                if ($j >= 99) {
+                    $result = $obj->is_csrf_valid($identify, $csrf_token);
+                    $this->assertTrue($result);
+                }
+            }
+        }
+
+        $this->assertTrue(isset($_SESSION[CsrfToken::PREFIX]));
+
+        if (isset($_SESSION[CsrfToken::PREFIX])) {
+            $this->assertTrue(is_array($_SESSION[CsrfToken::PREFIX]));
+
+            foreach($_SESSION[CsrfToken::PREFIX] as $area => $sessions)
+            {
+                $this->assertSame(50, count($sessions));
+                echo "Area Name: {$area}\n";
+                echo "Area Sessions Count: ".count($sessions)."\n";
+                print_r($sessions);
+                echo "---\n\n";
+            }
+        }
+
+        $obj->destroy_all();
+
+        $this->assertFalse(ArrayUtilus::haveData($_SESSION[CsrfToken::PREFIX]));
     }
 }
