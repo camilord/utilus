30779f3f5194daf82176e0549dadfd0bf2a3721c
camilo3rd <camilord@abcs.co.nz>
2022-04-28 10:48:06 +1200

    new functions in SystemUtilus.php - get mac address of the hosts - if commend exists

---
+++ tests/IO/SystemUtilusTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Developer: Camilo Lozano III - www.camilord.com
+ *                              - github.com/camilord
+ *                              - linkedin.com/in/camilord
+ *
+ * utilus - SystemUtilusTest.php
+ * Username: Camilo
+ * Date: 28/04/2022
+ * Time: 10:45 AM
+ */
+
+namespace camilord\utilus\IO;
+
+use camilord\utilus\IO\SystemUtilus;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class SystemUtilusTest
+ * @package camilord\utilus\IO
+ */
+class SystemUtilusTest extends TestCase
+{
+    public function testGetMacAddress() {
+        $mac_addresses = SystemUtilus::getHostMacAddress();
+        $this->assertTrue(is_array($mac_addresses));
+
+        foreach($mac_addresses as $mac_address)
+        {
+            $this->assertTrue(filter_has_var($mac_address, FILTER_VALIDATE_MAC));
+        }
+    }
+}
