27a38e44afbee8e47d7a633aa0bd41026198f7c9
Camilo3rd <camilo3rd@gmail.com>
2024-04-08 07:40:48 +0930

    Update DateTimeUtil.php
    microtime as float util and now util as simplified static calls

--- src/String/DateTimeUtil.php
+++ src/String/DateTimeUtil.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace camilord\utilus\String;
+
+use DateTime;
+
+class DateTimeUtil 
+{
+    const DEFAULT_FORMAT = 'Y-m-d H:i:s';
+
+    public static function now(): string
+    {
+        return (new DateTime())->format(self::DEFAULT_FORMAT);
+    }
+
+    public static function microtime_as_float(string $s): float
+    {
+        $dt = new DateTime($s);
+        return (float)$dt->format('U.u');
+    }
+}
